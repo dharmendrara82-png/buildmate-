d893323bb8a401f092b94f1509a3611297ee41c6
dharmendrara82-png <dharmendrara82@gmail.com>
2026-08-02 21:59:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("BuildMate Loaded");
+function calculateTile() {
+    let length = parseFloat(document.getElementById("length").value);
+    let width = parseFloat(document.getElementById("width").value);
+
+    if (isNaN(length) || isNaN(width)) {
+        document.getElementById("result").innerHTML =
+        "Please enter both values.";
+        return;
+    }
+
+    let area = length * width;
+
+    document.getElementById("result").innerHTML =
+    "Room Area = " + area + " sq.ft";
+}
